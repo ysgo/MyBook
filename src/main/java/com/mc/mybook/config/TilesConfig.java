00f1f4459350b7ccdb251ae43678f1651a300520
ysgo <wjion8428@gmail.com>
2020-04-12 23:48:12 +0900

    add tiles library and create tiles java config, xml

---
+++ src/main/java/com/mc/mybook/config/TilesConfig.java
@@ -0,0 +1,37 @@
+package com.mc.mybook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+@Configuration
+public class TilesConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public TilesConfigurer tilesConfigurer() {
+		TilesConfigurer configurer = new TilesConfigurer();
+		
+		// Tiles 설정 파일 위치한 디렉토리 + 파일명
+		configurer.setDefinitions(new String[] {"/WEB-INF/tiles/tiles.xml"});
+		configurer.setCheckRefresh(true);
+		return configurer;
+	}
+	
+	@Bean
+	public TilesViewResolver tilesViewResolver() {
+		final TilesViewResolver tilesViewResolver = new TilesViewResolver();
+		tilesViewResolver.setViewClass(TilesView.class);
+		tilesViewResolver.setOrder(1);	// 뷰 우선순위
+		return tilesViewResolver;
+	}
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		TilesViewResolver viewResolver = new TilesViewResolver();
+		registry.viewResolver(viewResolver);
+	}
+}
