f1d76c01a5995e11d6fb28f04a00c721e7bf1b96
ysgo <wjion8428@gmail.com>
2020-04-21 23:31:54 +0900

    flyway alter tables rename column

---
+++ src/main/resources/db/migration/V202004212250__rename_books_table.sql
@@ -0,0 +1,2 @@
+ALTER TABLE books CHANGE name title VARCHAR(255);
+ALTER TABLE books CHANGE content description VARCHAR(255);
---
+++ src/main/resources/db/migration/V202004212315__rename_reviews_table.sql
@@ -0,0 +1,1 @@
+ALTER TABLE reviews CHANGE content description VARCHAR(255);
